883029d3005a1be6a874bad3380993499ca81222
Paweł Kołodziejczyk <p.kolodziej92@gmail.com>
2021-03-29 15:30:10 +0200

    63. Building a “sticky” navigation - Part 2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log("hello");
-
 // $(document).ready(function () {
 //   $("h1").click(function () {
 //     $(this).css("background-color", "#ff0000");
@@ -11,37 +9,59 @@ console.log("hello");
 const nav = document.querySelector("nav");
 const logo = document.querySelector(".logo");
 
-nav.addEventListener("transitionend", function (e) {
-  if (this.classList.contains("sticky-fade-out")) {
-    this.classList.remove("sticky-fade-out");
-    this.classList.remove("sticky");
+// nav.addEventListener("transitionend", function (e) {
+//   if (this.classList.contains("sticky-fade-out")) {
+//     this.classList.remove("sticky-fade-out");
+//     this.classList.remove("sticky");
+//   }
+// });
+
+// document.querySelector("h1").addEventListener("click", function () {
+//   this.style.backgroundColor = "red";
+// });
+
+// let options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.2,
+// };
+
+// let observer = new IntersectionObserver(function (entries, observer) {
+//   //   console.log("visible");
+//   console.log(entries, observer);
+//   if (!entries[0].isIntersecting) {
+//     // console.log("visible");
+//     nav.classList.add("sticky");
+//     nav.classList.add("sticky-fade-in");
+
+//     logo.src = "../assets/logos/logo.png";
+//   } else if (entries[0].isIntersecting && nav.classList.contains("sticky")) {
+//     nav.classList.remove("sticky-fade-in");
+//     nav.classList.add("sticky-fade-out");
+//     logo.src = "../assets/logos/logo-white.png";
+//   }
+// });
+
+// observer.observe(document.querySelector("header"));
+
+//STICKY JQUERY
+
+$(".js--section-features").waypoint(
+  function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  },
+  {
+    offset: "60px;",
   }
-});
+);
 
-document.querySelector("h1").addEventListener("click", function () {
-  this.style.backgroundColor = "red";
-});
-
-let options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.2,
-};
-
-let observer = new IntersectionObserver(function (entries, observer) {
-  //   console.log("visible");
-  console.log(entries, observer);
-  if (!entries[0].isIntersecting) {
-    // console.log("visible");
-    nav.classList.add("sticky");
-    nav.classList.add("sticky-fade-in");
-
-    logo.src = "../assets/logos/logo.png";
-  } else if (entries[0].isIntersecting && nav.classList.contains("sticky")) {
-    nav.classList.remove("sticky-fade-in");
-    nav.classList.add("sticky-fade-out");
-    logo.src = "../assets/logos/logo-white.png";
-  }
-});
-
-observer.observe(document.querySelector("header"));
+// var waypoint = new Waypoint({
+//   element: document.getElementById("basic-waypoint"),
+//   handler: function () {
+//     notify("Basic waypoint triggered");
+//   },
+// });
